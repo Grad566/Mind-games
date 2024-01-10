2205c8951974a474b0326b2638a8593d2aa7afdb
Denis Orekhov <3008denis@gmail.com>
2024-01-10 14:26:13 +0500

    Refactoring Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,21 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static void cheers() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        System.out.println("Hello, " + getName() + "!");
+    }
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();
+
+        scanner.close();
+
+        return userName;
+    }
+
 }
