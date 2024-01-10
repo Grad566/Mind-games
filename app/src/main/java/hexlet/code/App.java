ec8c6e560018353449f45cc17f7066d2f7624741
Denis Orekhov <3008denis@gmail.com>
2024-01-10 14:24:17 +0500

    Make method main in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
+    // метод реализует меню и выбор игры
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet \n2 - Even \n0 - Exit");
+        int choice = scanner.nextInt();
+        System.out.println("You choice: " + choice);
+        System.out.println();
+
+        switch (choice) {
+            case 1:
+                Greeting.cheers();
+            case 2:
+                EvenGame.playEvenGame();
+            default:
+                break;
+        }
     }
 }
