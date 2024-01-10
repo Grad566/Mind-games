115d4ca5b497a2206c0894a2bfd46b01b592723f
Denis Orekhov <3008denis@gmail.com>
2024-01-10 14:29:16 +0500

    Create Greeting.java and add some coe

--- app/src/main/java/hexlet/code/Greeting.java
+++ app/src/main/java/hexlet/code/Greeting.java
@@ -1,2 +1,20 @@
-package hexlet.code;public class Greeting {
+package hexlet.code;
+import java.util.Scanner;
+
+public class Greeting {
+    // выводит привественное сообщение
+    public static void cheers() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        System.out.println("Hello, " + getName() + "!");
+    }
+    // получаем имя пользователя
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+
+        String userName = scanner.next();
+
+        return userName;
+    }
+
 }
