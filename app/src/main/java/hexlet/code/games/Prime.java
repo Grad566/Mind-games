63b8ac0847c3b39c72feeb32548e283a66be1372
Denis Orekhov <3008denis@gmail.com>
2024-01-12 11:12:38 +0500

    Make methods in Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,2 +1,43 @@
-package hexlet.code.games;public class Prime {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+public class Prime {
+
+    public static void playPrime() {
+        var victory = true;
+
+        Engine.cheers();
+
+        System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
+
+        for (var i = 0; i < 3; i++) {
+
+            if (!victory) {
+                break;
+            }
+
+            var randomNumber = Engine.getRandomNumber(200);
+            var correctAnswer = getCorrectAnswer(randomNumber);
+
+            System.out.println("Question: "
+                    + randomNumber);
+
+            victory = Engine.checkAnswer(correctAnswer);
+
+        }
+
+        Engine.checkVictory(victory);
+    }
+
+    public static String getCorrectAnswer(int number) {
+
+        for (var i = 2; i <= (number / 2); i++) {
+            if (number % i == 0) {
+                return "no";
+            }
+        }
+
+        return "yes";
+    }
 }
