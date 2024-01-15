8a15c748b1f0738b2683a68f659209217119a195
Denis Orekhov <3008denis@gmail.com>
2024-01-15 13:01:35 +0500

    Refactoring Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -3,7 +3,8 @@ package hexlet.code.games;
 import hexlet.code.Engine;
 
 public class Calc {
-    // запускаем саму игру и привественное сообщение
+    // выводим привественное сообщение
+    // запускаем игру
     public static void playCalcGame() {
         final int countMathSigns = 3;
         final int maxNumberGenerated = 500;
@@ -14,50 +15,16 @@ public class Calc {
 
         String[][] questionsAndAnswers = new String[Engine.getCountOfQuestions()][2];
 
+        // генерируем массив из вопросов и ответов
         for (var i = 0; i < Engine.getCountOfQuestions(); i++) {
-            var mathSign = getMathSign(Engine.getRandomNumber(countMathSigns));
+            var mathSign = Engine.getMathSign(Engine.getRandomNumber(countMathSigns));
             var firstNumber = Engine.getRandomNumber(maxNumberGenerated);
             var secondNumber = Engine.getRandomNumber(maxNumberGenerated);
-            var correctAnswer = getExpressionResult(firstNumber, secondNumber, mathSign);
+            var correctAnswer = Engine.getExpressionResult(firstNumber, secondNumber, mathSign);
 
             questionsAndAnswers[i][0] = firstNumber + " " + mathSign + " " + secondNumber;
             questionsAndAnswers[i][1] = correctAnswer;
         }
         Engine.playGame(questionsAndAnswers);
-    }
-
-    // получаем случайный оператор
-    public static String getMathSign(int randomNumber) {
-        String mathSign = "";
-        switch (randomNumber) {
-            case 1:
-                mathSign = "+";
-                break;
-            case 2:
-                mathSign = "-";
-                break;
-            default:
-                mathSign = "*";
-                break;
-        }
-        return mathSign;
-    }
-
-    // получаем результат выражения
-    public static String getExpressionResult(int firstNumber, int secondNumber, String sign) {
-        return switch (sign) {
-            case "+" -> {
-                int sum = firstNumber + secondNumber;
-                yield String.valueOf(sum);
-            }
-            case "-" -> {
-                int difference = firstNumber - secondNumber;
-                yield String.valueOf(difference);
-            }
-            default -> {
-                int product = firstNumber * secondNumber;
-                yield String.valueOf(product);
-            }
-        };
     }
 }
