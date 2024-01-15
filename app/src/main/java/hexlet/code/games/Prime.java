f2b63e58f02aa22044c34704301fe3de12d7af34
Denis Orekhov <3008denis@gmail.com>
2024-01-15 12:04:42 +0500

    Refactoring meethod playPrime in Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -5,35 +5,22 @@ import hexlet.code.Engine;
 public class Prime {
     // запускает саму игру и привественное сообщение
     public static void playPrime() {
-        var victory = true;
         final int maxRandomNumber = 200;
 
         Engine.cheers();
 
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
 
-        // выводим пользователю число
-        // если ответ ложный, прекращаем игру и выводим сообщение о проигрыше
-        // повторяем трижды, либо до победы, либо до 1-го неправильного ответа
+        String[][] QuestionsAndAnswers = new String[Engine.getCountOfQuestions()][2];
+
         for (var i = 0; i < Engine.getCountOfQuestions(); i++) {
-
-            if (!victory) {
-                break;
-            }
-
             var randomNumber = Engine.getRandomNumber(maxRandomNumber);
             var correctAnswer = getCorrectAnswer(randomNumber);
 
-            System.out.println("Question: "
-                    + randomNumber);
-
-            victory = Engine.checkAnswer(correctAnswer);
-
+            QuestionsAndAnswers[i][0] = String.valueOf(randomNumber);
+            QuestionsAndAnswers[i][1] = correctAnswer;
         }
-
-        // проверяме условие победы
-        // выводим сообещние, если условие true
-        Engine.checkVictory(victory);
+        Engine.playGame(QuestionsAndAnswers);
     }
 
     // получаем правильный ответ (простое число или нет)
