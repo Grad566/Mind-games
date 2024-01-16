708cfd93dde1dd85d22136bed122bb5298795c1e
Denis Orekhov <3008denis@gmail.com>
2024-01-16 11:09:39 +0500

    Refactoring Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -17,14 +17,49 @@ public class Calc {
 
         // генерируем массив из вопросов и ответов
         for (var i = 0; i < Engine.getCountOfQuestions(); i++) {
-            var mathSign = Engine.getMathSign(Engine.getRandomNumber(countMathSigns));
+            var mathSign = getMathSign(Engine.getRandomNumber(countMathSigns));
             var firstNumber = Engine.getRandomNumber(maxNumberGenerated);
             var secondNumber = Engine.getRandomNumber(maxNumberGenerated);
-            var correctAnswer = Engine.getExpressionResult(firstNumber, secondNumber, mathSign);
+            var correctAnswer = getExpressionResult(firstNumber, secondNumber, mathSign);
 
             questionsAndAnswers[i][0] = firstNumber + " " + mathSign + " " + secondNumber;
             questionsAndAnswers[i][1] = correctAnswer;
         }
         Engine.playGame(questionsAndAnswers);
+    }
+
+    // получаем случайный математический знак
+    public static String getMathSign(int randomNumber) {
+        String mathSign = "";
+        switch (randomNumber) {
+            case 1:
+                mathSign = "+";
+                break;
+            case 2:
+                mathSign = "-";
+                break;
+            default:
+                mathSign = "*";
+                break;
+        }
+        return mathSign;
+    }
+
+    // получаем результат матем. выражения
+    public static String getExpressionResult(int firstNumber, int secondNumber, String sign) {
+        return switch (sign) {
+            case "+" -> {
+                int sum = firstNumber + secondNumber;
+                yield String.valueOf(sum);
+            }
+            case "-" -> {
+                int difference = firstNumber - secondNumber;
+                yield String.valueOf(difference);
+            }
+            default -> {
+                int product = firstNumber * secondNumber;
+                yield String.valueOf(product);
+            }
+        };
     }
 }
