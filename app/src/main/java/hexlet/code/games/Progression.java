e13fd9bb8d991e98baa1a038b4da95301718e31e
Denis Orekhov <3008denis@gmail.com>
2024-01-17 11:36:25 +0500

    Reafactoring Progression.java
    Add generateRoundDate
    Refactoring playProgression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,37 +1,21 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 public class Progression {
-    // выводим привественное сообщение
     // запускаем игру
     public static void playProgression() {
-        final int maxStepProgression = 8;
-
-        Engine.cheers();
-
-        System.out.println("What number is missing in the progression?");
-
-        String[][] questionsAndAnswers = new String[Engine.getCountOfQuestions()][2];
+        var rule = "What number is missing in the progression?";
+        String[][] questionsAndAnswers = new String[Utils.getCountOfQuestions()][2];
 
         // генерируем массив из вопросов и ответов
-        for (var i = 0; i < Engine.getCountOfQuestions(); i++) {
-            var step = Engine.getRandomNumber(maxStepProgression);
-            // получаем случайный массив с арифм. прогр.
-            String[] randomArray = getRandomArray(step);
-            // получаем номер элемента для замены
-            var randomIndex = Engine.getRandomNumber(randomArray.length - 1);
-            // записываем верный ответ
-            var correctAnswer = randomArray[randomIndex];
-            // получаем массив, который покажем пользователю
-            randomArray = getArrayForUser(randomArray, randomIndex);
-            String arrayAsString = String.join(" ", randomArray);
-
-            questionsAndAnswers[i][0] = arrayAsString;
-            questionsAndAnswers[i][1] = correctAnswer;
+        for (var i = 0; i < Utils.getCountOfQuestions(); i++) {
+            questionsAndAnswers[i] = generateRoundDate();
         }
-        Engine.playGame(questionsAndAnswers);
+        Engine.playGame(questionsAndAnswers, rule);
     }
+
     // меняем один элемент на ".."
     public static String[] getArrayForUser(String[] arrayNumbers, int randomIndex) {
         arrayNumbers[randomIndex] = "..";
@@ -41,10 +25,10 @@ public class Progression {
     // получаем случайный массив с ариф. прогрессией
     public static String[] getRandomArray(int step) {
         final int maxInitialNumber = 100;
-        var initialNumber = Engine.getRandomNumber(maxInitialNumber);
         final int minLength = 5;
         final int averageArrayLength = 10;
-        String[] arrayNumbers = new String[Engine.getRandomNumber(averageArrayLength) + minLength];
+        var initialNumber = Utils.getRandomNumber(maxInitialNumber);
+        String[] arrayNumbers = new String[Utils.getRandomNumber(averageArrayLength) + minLength];
         var arrayLength = arrayNumbers.length;
 
         for (var i = 0; i < arrayLength; i++) {
@@ -52,5 +36,28 @@ public class Progression {
             initialNumber += step;
         }
         return arrayNumbers;
+    }
+
+    // Генерируем вопрос и ответ на один раунд
+    public static String[] generateRoundDate() {
+        final int maxStepProgression = 8;
+
+        String[] roundDate = new String[2];
+
+        var step = Utils.getRandomNumber(maxStepProgression);
+        // получаем случайный массив с арифм. прогр.
+        String[] randomArray = getRandomArray(step);
+        // получаем номер элемента для замены
+        var randomIndex = Utils.getRandomNumber(randomArray.length - 1);
+        // записываем верный ответ
+        var correctAnswer = randomArray[randomIndex];
+        // получаем массив, который покажем пользователю
+        randomArray = getArrayForUser(randomArray, randomIndex);
+        String arrayAsString = String.join(" ", randomArray);
+
+        roundDate[0] = arrayAsString;
+        roundDate[1] = correctAnswer;
+
+        return roundDate;
     }
 }
