4c5466f2c4d41ea00f1dce56cdf9e4e618608a68
Denis Orekhov <3008denis@gmail.com>
2024-01-17 11:22:49 +0500

    Now have one methood in Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,19 +3,8 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+    // Вызываем приветсвие
     public static void cheers() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
-        System.out.println("Hello, " + getName() + "!");
+        Utils.cheers();
     }
-    public static String getName() {
-        Scanner scanner = new Scanner(System.in);
-
-        String userName = scanner.next();
-
-        scanner.close();
-
-        return userName;
-    }
-
 }
