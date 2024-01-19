76582f11997f3e4234a8db7c24b7a3100a48ae83
Denis Orekhov <3008denis@gmail.com>
2024-01-19 17:00:34 +0500

    Refactoring isEven in Prime.java

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -19,6 +19,9 @@ public class Prime {
 
     // Проверям простое ли число
     public static boolean isPrime(int number) {
+        if (number == 1) {
+            return false;
+        }
         for (var i = 2; i <= (number / 2); i++) {
             if (number % i == 0) {
                 return false;
