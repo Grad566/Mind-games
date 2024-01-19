caec6343346be48c96024f58ac7c9086a3488936
Denis Orekhov <3008denis@gmail.com>
2024-01-19 14:26:11 +0500

    Delete var userName and constructor in Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,21 +3,14 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    private static String userName;
-
-    Cli(String inputUserName) {
-        Cli.userName = inputUserName;
-    }
-
     // Вызываем приветсвие
     public static String cheers() {
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        if (userName == null) {
-            Scanner scanner = new Scanner(System.in);
 
-            userName = scanner.next();
-        }
+        Scanner scanner = new Scanner(System.in);
+        var userName = scanner.next();
+
         System.out.println("Hello, " + userName + "!");
 
         return userName;
