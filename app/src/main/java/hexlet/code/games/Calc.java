4047730cfe701ee3d728a8c7b1762de72c1f9707
Denis Orekhov <3008denis@gmail.com>
2024-01-19 16:02:21 +0500

    Add new conts to Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,6 +5,9 @@ import hexlet.code.Utils;
 
 public class Calc {
     public static final int ROUNDS_COUNT = 3;
+    public static final int COUNT_MATH_SIGNS = 3;
+    public static final int MAX_NUMBER_GENERATED = 500;
+
     public static final String RULE = "What is the result of the expression?";
     // запускаем игру
     public static void playCalcGame() {
@@ -34,13 +37,11 @@ public class Calc {
 
     // Генерируем вопрос и ответ на один раунд
     public static String[] generateRoundData() {
-        final int countMathSigns = 3;
-        final int maxNumberGenerated = 500;
         String[] roundDate = new String[2];
 
-        var mathSign = getMathSign(Utils.getRandomNumber(countMathSigns));
-        var firstNumber = Utils.getRandomNumber(maxNumberGenerated);
-        var secondNumber = Utils.getRandomNumber(maxNumberGenerated);
+        var mathSign = getMathSign(Utils.getRandomNumber(COUNT_MATH_SIGNS));
+        var firstNumber = Utils.getRandomNumber(MAX_NUMBER_GENERATED;
+        var secondNumber = Utils.getRandomNumber(MAX_NUMBER_GENERATED);
         var correctAnswer = String.valueOf(getExpressionResult(firstNumber, secondNumber, mathSign));
 
         roundDate[0] = firstNumber + " " + mathSign + " " + secondNumber;
